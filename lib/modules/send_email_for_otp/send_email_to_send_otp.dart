5a02d8e4148c623857e5522786f3e6dae22319a1
mohamedsabra0 <mohamedsabra0110@gmail.com>
2024-06-26 13:05:28 +0300

    push

--- lib/modules/send_email_for_otp/send_email_to_send_otp.dart
+++ lib/modules/send_email_for_otp/send_email_to_send_otp.dart
@@ -1,5 +1,6 @@
 import 'package:councils/modules/code_verify_screen/code_verify.dart';
-import 'package:councils/modules/send_email_for_otp/cubit/cubit.dart';
+import 'package:councils/modules/forget_password_screen/cubit/cubit.dart';
+import 'package:councils/modules/forget_password_screen/cubit/states.dart';
 import 'package:councils/shared/component/component.dart';
 import 'package:councils/shared/component/constants.dart';
 import 'package:councils/shared/network/local/cache_helper.dart';
@@ -8,30 +9,26 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
-import 'cubit/states.dart';
-
 class SendEmailToSendOTP extends StatelessWidget {
   //const SendEmailToSendOTP({super.key});
-  var emailController=TextEditingController();
-  var formKey=GlobalKey<FormState>();
+  var emailController = TextEditingController();
+  var formKey = GlobalKey<FormState>();
   @override
   Widget build(BuildContext context) {
     return BlocProvider(
-      create: (context)=>ForgetPassCubit(),
-      child: BlocConsumer<ForgetPassCubit,ForgetPassStates>(
-        listener: (BuildContext context,  state) {
-          if(state is ForgetPassSuccessState)
-            {
+      create: (context) => ForgetPassCubit(),
+      child: BlocConsumer<ForgetPassCubit, ForgetPassStates>(
+        listener: (BuildContext context, state) {
+          if (state is ForgetPassSuccessState) {
             //  CacheHelper.saveData(key: 'token', value: state.loginModel.token);
             //  token=state.loginModel.token!;
-              Navigator.pushAndRemoveUntil(
-                  context,
-                  MaterialPageRoute(builder: (context)=>CodeVerifyScreen()),
-                  (route) => false
-              );
-            }
+            Navigator.pushAndRemoveUntil(
+                context,
+                MaterialPageRoute(builder: (context) => CodeVerifyScreen()),
+                (route) => false);
+          }
         },
-        builder: (BuildContext context,  state) {
+        builder: (BuildContext context, state) {
           return Scaffold(
             body: Column(
               mainAxisAlignment: MainAxisAlignment.center,
@@ -40,31 +37,26 @@ class SendEmailToSendOTP extends StatelessWidget {
                   key: formKey,
                   child: defaultTextfield(
                       controller: emailController,
-                      validate: (value)
-                      {
-                        if(value.isEmpty)
-                        {
+                      validate: (value) {
+                        if (value.isEmpty) {
                           return "email must not be empty";
                         }
                       },
-                      hintText:'Email',
+                      hintText: 'Email',
                       prefix: Icons.email,
-                      type: TextInputType.emailAddress
-                  ),
+                      type: TextInputType.emailAddress),
                 ),
                 SizedBox(
                   height: 50.h,
                 ),
                 defaultBottom(
-                    radius:30,
+                    radius: 30,
                     color: Color(0xff2752e7),
-                    text:'send code',
-                    onpressed: (){
-                      if(formKey.currentState!.validate())
-                      {
-                        ForgetPassCubit.get(context).forgetPass(
-                            email: emailController.text
-                        );
+                    text: 'send code',
+                    onpressed: () {
+                      if (formKey.currentState!.validate()) {
+                        ForgetPassCubit.get(context)
+                            .forgetPass(email: emailController.text);
                         // UserLoginCubit.get(context).userLogin(
                         //     email: emailController.text,
                         //     password: passwordController.text
@@ -74,16 +66,12 @@ class SendEmailToSendOTP extends StatelessWidget {
                         //     MaterialPageRoute(builder: (context)=> CodeVerifyScreen())
                         // );
                       }
-
-                    }
-                )
+                    })
               ],
             ),
           );
         },
-
       ),
     );
-
   }
 }
